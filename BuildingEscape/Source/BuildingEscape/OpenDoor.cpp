ec560290b28fdbee5cbda59ce66472d30ae6c28f
MARK-A15\mac10 <mac101010@gmail.com>
2016-09-19 21:30:08 -0700

    (Section 3, Lecture 61) Basic room with walls, doorway, and test log messages

---
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BuildingEscape.h"
+#include "OpenDoor.h"
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	bWantsBeginPlay = true;
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	AActor* Owner = GetOwner();
+
+	// FRotator is pitch, yaw, and roll
+	FRotator NewRotation = FRotator(0,-60,0);
+	Owner->SetActorRotation(NewRotation);
+
+	UE_LOG(LogTemp, Warning, TEXT("%s is at %s"), *Owner->GetName(), *(Owner->GetTransform().GetRotation().ToString()));	
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+{
+	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
+
+	// ...
+}
+
